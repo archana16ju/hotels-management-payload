846f86909522bdf7cbcb6aaf83f35d5df19825b6
archana16ju <archana.work.in@gmail.com>
2026-03-09 16:09:15 +0530

    Initial commit

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,21 +1,35 @@
 import type { CollectionConfig } from 'payload'
+import { v2 as cloudinary } from 'cloudinary'
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+})
 
 export const Media: CollectionConfig = {
   slug: 'media',
+
   access: {
     read: () => true,
     create: () => true,
     update: () => true,
     delete: () => true,
   },
-  upload: {
-    staticDir: 'media',
-  },
+
+  upload: false, 
+
   fields: [
+    {
+      name: 'image',
+      type: 'text',
+      admin: {
+        description: 'Cloudinary Image URL',
+      },
+    },
     {
       name: 'alt',
       type: 'text',
-      required: true,
     },
   ],
 }
--- src/payload-types.ts
+++ src/payload-types.ts
@@ -167,18 +167,13 @@ export interface User {
  */
 export interface Media {
   id: string;
-  alt: string;
+  /**
+   * Cloudinary Image URL
+   */
+  image?: string | null;
+  alt?: string | null;
   updatedAt: string;
   createdAt: string;
-  url?: string | null;
-  thumbnailURL?: string | null;
-  filename?: string | null;
-  mimeType?: string | null;
-  filesize?: number | null;
-  width?: number | null;
-  height?: number | null;
-  focalX?: number | null;
-  focalY?: number | null;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -515,18 +510,10 @@ export interface UsersSelect<T extends boolean = true> {
  * via the `definition` "media_select".
  */
 export interface MediaSelect<T extends boolean = true> {
+  image?: T;
   alt?: T;
   updatedAt?: T;
   createdAt?: T;
-  url?: T;
-  thumbnailURL?: T;
-  filename?: T;
-  mimeType?: T;
-  filesize?: T;
-  width?: T;
-  height?: T;
-  focalX?: T;
-  focalY?: T;
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
