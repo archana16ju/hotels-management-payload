d0f6c57dc9d2707eaa0351e2131402d99e85bcd3
archana16ju <archana.work.in@gmail.com>
2026-03-10 18:05:39 +0530

    Initial commit

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,27 +1,28 @@
-import type { CollectionConfig } from 'payload';
+import type { CollectionConfig } from 'payload'
+
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const filename = fileURLToPath(import.meta.url)
+const dirname = path.dirname(filename)
 
 export const Media: CollectionConfig = {
   slug: 'media',
+
   admin: {
-    useAsTitle: 'name',
+    useAsTitle: 'alt',
   },
   access: {
-    read: () => true, 
+    read: () => true,
+    create: () => true,
+    update: () => true,
+    delete: () => true,
   },
   fields: [
     {
-      name: 'name',
+      name: 'alt',
       type: 'text',
       required: true,
     },
-    {
-      name: 'alt',
-      type: 'text',
-    },
-    {
-      name: 'file',
-      type: 'upload',
-      relationTo: 'media',
-    },
   ],
-};
+}
