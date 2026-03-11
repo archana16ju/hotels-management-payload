6638bc645c7577c472730b85b0432ddec8fcace5
archana16ju <archana.work.in@gmail.com>
2026-03-11 18:01:09 +0530

    Initial commit

--- src/payload-types.ts
+++ src/payload-types.ts
@@ -167,7 +167,7 @@ export interface User {
  */
 export interface Media {
   id: string;
-  alt: string;
+  alt?: string | null;
   updatedAt: string;
   createdAt: string;
   url?: string | null;
@@ -179,16 +179,6 @@ export interface Media {
   height?: number | null;
   focalX?: number | null;
   focalY?: number | null;
-  sizes?: {
-    thumbnail?: {
-      url?: string | null;
-      width?: number | null;
-      height?: number | null;
-      mimeType?: string | null;
-      filesize?: number | null;
-      filename?: string | null;
-    };
-  };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
@@ -250,7 +240,10 @@ export interface Product {
 export interface Category {
   id: string;
   name: string;
-  slug?: string | null;
+  /**
+   * Auto-generated from name, lowercase, no spaces or special chars
+   */
+  slug: string;
   parent?: (string | null) | Category;
   image?: (string | null) | Media;
   description?: string | null;
@@ -534,20 +527,6 @@ export interface MediaSelect<T extends boolean = true> {
   height?: T;
   focalX?: T;
   focalY?: T;
-  sizes?:
-    | T
-    | {
-        thumbnail?:
-          | T
-          | {
-              url?: T;
-              width?: T;
-              height?: T;
-              mimeType?: T;
-              filesize?: T;
-              filename?: T;
-            };
-      };
 }
 /**
  * This interface was referenced by `Config`'s JSON-Schema
